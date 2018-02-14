05b7e42be31ff82347fd6e64526bb8ba71c0936f
Ildar <dilmuha@mail.ru>
2018-02-14 11:23:39 +0300

    multithreading
    multithreading with timer realised

--- car_imitator/car_imitator.ino
+++ car_imitator/car_imitator.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+//#include <SimpleTimer.h>
 
 
 #define FORWARD 2// двигатель вперед
@@ -6,6 +7,7 @@
 #define LIGTHS 4//фары
 //#define BRAKE 5//тормоз
 #define HELM 6
+
 
 
 //кнопки для имитации поступления сигнала
@@ -19,97 +21,124 @@
 
 Servo helm;//имитация руля
 int helmAngle = 90;
+unsigned long int timeMillis, timePreview;
+bool ON;
 
-void setup() 
+
+void setup()
 {
-
   Serial.begin(9600);
   pinMode(FORWARD, OUTPUT);
   pinMode(BACK, OUTPUT);
   pinMode(LIGTHS, OUTPUT);
   pinMode(HELM, OUTPUT);
-//  pinMode(BRAKE, OUTPUT);
-  
-  pinMode(FORWARD_BUTTON,  INPUT_PULLUP);
-  pinMode(BACK_BUTTON, INPUT_PULLUP);
-  pinMode(LIGTH_BUTTON, INPUT_PULLUP);
-  pinMode(BRAKE_BUTTON, INPUT_PULLUP);
-  pinMode(HELM_RIGTH_BUTTON, INPUT_PULLUP);
-  pinMode(HELM_LEFT_BUTTON, INPUT_PULLUP);
-
+  //  pinMode(BRAKE, OUTPUT);
+  /*
+    pinMode(FORWARD_BUTTON,  INPUT_PULLUP);
+    pinMode(BACK_BUTTON, INPUT_PULLUP);
+    pinMode(LIGTH_BUTTON, INPUT_PULLUP);
+    pinMode(BRAKE_BUTTON, INPUT_PULLUP);
+    pinMode(HELM_RIGTH_BUTTON, INPUT_PULLUP);
+    pinMode(HELM_LEFT_BUTTON, INPUT_PULLUP);
+  */
   helm.attach(HELM);
-  
+
   MoveToAngle(helmAngle);//начальное положение руля - колеса прямо
-  
-}
 
-void loop() 
+}
+/*
+  1."forward" - едем вперед
+  2."back" - едем назад
+  3."motor off" - выключить мотор
+  4."ligth on" - фары включить
+  5."ligth off" - фары выключить
+  6."helm left" - лево руля (вешаем на прерывание)
+  7."helm rigth" - право руля(вешаем на прерывание)
+*/
+int incomingByte = 0;
+void loop()
 {
- if(!digitalRead(FORWARD_BUTTON)) MotorForward();
- 
- if(!digitalRead(BACK_BUTTON)) MotorBack();
 
- if(digitalRead(BACK_BUTTON) && digitalRead(FORWARD_BUTTON))  MotorOff();
- 
- if(!digitalRead(LIGTH_BUTTON)) LigthOn();
- else LigthOff();
-
- //if(!digitalRead(BRAKE_BUTTON)) BrakOn();
-
-if(!digitalRead(HELM_RIGTH_BUTTON)) MoveHelmToRigth();
-if(!digitalRead(HELM_LEFT_BUTTON)) MoveHelmToLeft();
-
- 
+  if (Serial.available() > 0) {
+    // считываем входящий байт:
+    incomingByte = Serial.read();
+  }
+  if (timeMillis % 100 == 0)
+  {
+    //Serial.println(100);
+    timePreview = millis();
+    if (incomingByte==54) MoveHelmToRigth();
+    if (incomingByte==55) MoveHelmToLeft();
+   
+  }
+  if (timeMillis % 500 == 0)
+  {
+   //Serial.println(500);
+    timePreview = millis();
+    if (incomingByte==49) MotorForward();
+    if (incomingByte==50) MotorBack();
+    if (incomingByte==51) MotorOff();
+  }
+  if (timeMillis % 1000 == 0)
+  {
+    //Serial.println(1000);
+    timePreview = millis();
+    if (incomingByte==52) LigthOn();
+    if (incomingByte==53) LigthOff();
+  }
+  incomingByte = 0;
+  Serial.flush();
 }
 
-void MotorForward(){
-  digitalWrite(BACK,LOW);
- // delay(1);
-  digitalWrite(FORWARD,HIGH);
+void MotorForward() {
+  digitalWrite(BACK, LOW);
+  // delay(1);
+  digitalWrite(FORWARD, HIGH);
   Serial.println("Едем вперед ВЖЖЖЖЖЖ...");
 }
-void MotorBack(){
-  digitalWrite(FORWARD,LOW);
-   //delay(1);
+void MotorBack() {
+  digitalWrite(FORWARD, LOW);
+  //delay(1);
   digitalWrite(BACK, HIGH);
   Serial.println("Едем назад ЖВВВВВВВ...");
 }
 
-void MotorOff(){
-   digitalWrite(FORWARD,LOW);
-   digitalWrite(BACK, LOW);
-   Serial.println("Стоим((");
+void MotorOff() {
+  digitalWrite(FORWARD, LOW);
+  digitalWrite(BACK, LOW);
+  Serial.println("Стоим((");
 }
-void LigthOn(){
- digitalWrite(LIGTHS, HIGH);
- Serial.println("Фары включены");
+void LigthOn() {
+  digitalWrite(LIGTHS, HIGH);
+  Serial.println("Фары включены");
 }
-void LigthOff(){
- digitalWrite(LIGTHS, LOW);
-// Serial.print("Фары выключены");
+void LigthOff() {
+  digitalWrite(LIGTHS, LOW);
+  // Serial.print("Фары выключены");
 }
-void BrakOff(){
-// digitalWrite(BRAKE,HIGH);//потому что пнп транзистор, других тупо не было
+void BrakOff() {
+  // digitalWrite(BRAKE,HIGH);//потому что пнп транзистор, других тупо не было
 }
-void BrakOn(){
-// digitalWrite(LIGTHS,LOW);//потому что пнп транзистор, других тупо не было
+void BrakOn() {
+  // digitalWrite(LIGTHS,LOW);//потому что пнп транзистор, других тупо не было
 }
 //во избежании вской херни, работать с рулем через методы moveHelmToLeft и Rigth, ну или через геттеры и сеттеры на крайняк
-void MoveHelmToLeft(){
-  int angle = getHelmAngle()+20;
+void MoveHelmToLeft() {
+  int angle = getHelmAngle() + 20;
   setHelmAngle(angle);
   helm.write(getHelmAngle());
   Serial.println("Лево руля");
+  
 }
 
-void MoveHelmToRigth(){
-  int angle = getHelmAngle()-20;
+void MoveHelmToRigth() {
+  int angle = getHelmAngle() - 20;
   setHelmAngle(angle);
   helm.write(getHelmAngle());
   Serial.println("Право руля");
 }
 
-void MoveToAngle(int angle){
+void MoveToAngle(int angle) {
   helm.write(angle);
   setHelmAngle(angle);
   Serial.println("Руль в положении ");
@@ -117,12 +146,12 @@ void MoveToAngle(int angle){
   Serial.println(" градусов");
 }
 
-int getHelmAngle(){
+int getHelmAngle() {
   return helmAngle;
 }
-void setHelmAngle(int angle){
-  if(angle>180 || angle<0)
-  return;
+void setHelmAngle(int angle) {
+  if (angle > 180 || angle < 0)
+    return;
   else (helmAngle = angle);
 }
 
